60f374d9a6faa9108f780358a794f6ad720eb034
Pradeep1308 <61630284+Pradeep1308@users.noreply.github.com>
2020-03-30 15:29:44 +0530

    update2

--- demand_paging.c
+++ demand_paging.c
@@ -11,3 +11,18 @@ double mstomis(double b)
 	b=b*1000;
 	return b;
 }
+int main()
+{
+double result;
+double SPF;
+double RPNM;
+double MAT;
+double EAT;
+double c;
+double d;
+double p;
+
+
+printf("Enter the value of Replace Page Not Modified(RPNM) in millisecond:\n");
+scanf("%lf",&RPNM);
+RPNM=mstons(RPNM);
