aa6b7ab9e3cd0fb1afc54feacb4a9c8166a88897
Pradeep1308 <61630284+Pradeep1308@users.noreply.github.com>
2020-03-30 15:25:02 +0530

    Update Fibonacci_sequence.c

--- Fibonacci_sequence.c
+++ Fibonacci_sequence.c
@@ -12,3 +12,16 @@ typedef struct {
 long fib_sequence[MAX_SEQUENCE];
 int sequence_size;
 } shared_data;
+int main( int argc,char *argv[] ){
+int a=0,b=1,n=a+b,i,ii;
+int pid;
+int segment_id;
+shared_data *shared_memory;
+const int size = sizeof(shared_data);
+
+segment_id = shmget ( IPC_PRIVATE, size, S_IRUSR | S_IWUSR);
+shared_memory = (shared_data*) shmat (segment_id, NULL, 0);
+
+
+printf("Please enter a number:\n");
+scanf(" %d", &ii);
